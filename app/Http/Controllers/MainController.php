7e5c00604c21f721a9b49c349cc291136bc90274
ahmmed atef <ahmed.web94@gmail.com>
2021-10-25 22:08:23 +0200

    first

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Filter\Filter;
 use App\Http\Filter\QueryFilter;
-use App\Http\Requests\FilterRequest;
 use Illuminate\Support\Facades\Http;
 
 class MainController extends Controller
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\MainController::class,'index']);
 
-Route::get('test', [\App\Http\Controllers\MainController::class,'index']);
+//Route::get('test', [\App\Http\Controllers\MainController::class,'index']);
